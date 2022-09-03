9cb68a8c90fcfdfc9b90631d761725ff68beff4c
wander217 <Trinhthinh2171999@gmail.com>
2022-09-03 17:35:19 +0700

    fix dataset

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -33,44 +33,44 @@ class PRENDataset(Dataset):
         self.txn: List = []
         self.nSample: int = 0
         self.alphabet = alphabet
-        for file in listdir(path):
-            env = lmdb.open(join(path, file),
-                            max_readers=8,
-                            readonly=True,
-                            lock=False,
-                            readahead=False,
-                            meminit=False)
-            txn = env.begin(write=False)
-            nSample: int = int(txn.get('num-samples'.encode()))
-            self.txn.append({
-                "txn": txn,
-                "nSample": nSample
-            })
-            self.nSample += nSample
-        self.index: np.ndarray = np.zeros((self.nSample, 2), dtype=np.int32)
-        start: int = 0
-        for i, item in enumerate(self.txn):
-            nSample = item['nSample']
-            self.index[start:start + nSample, 0] = i
-            self.index[start:start + nSample, 1] = np.arange(nSample, dtype=np.int32) + 1
-            start += nSample
+        # for file in listdir(path):
+        env = lmdb.open(path,
+                        max_readers=8,
+                        readonly=True,
+                        lock=False,
+                        readahead=False,
+                        meminit=False)
+        self._txn = env.begin(write=False)
+        # nSample: int = int(txn.get('num-samples'.encode()))
+        # self.txn.append({
+        #     "txn": txn,
+        #     "nSample": nSample
+        # })
+        # self.nSample += nSample
+        # self.index: np.ndarray = np.zeros((self.nSample, 2), dtype=np.int32)
+        # start: int = 0
+        # for i, item in enumerate(self.txn):
+        #     nSample = item['nSample']
+        #     self.index[start:start + nSample, 0] = i
+        #     self.index[start:start + nSample, 1] = np.arange(nSample, dtype=np.int32) + 1
+        #     start += nSample
 
     def __len__(self):
         return self.nSample
 
     def __getitem__(self, index: int) -> Tuple:
-        txn_id, rid = self.index[index]
-        txn = self.txn[txn_id]['txn']
+        # txn_id, rid = self.index[index]
+        # txn = self.txn[txn_id]['txn']
 
-        img_code: str = 'image-%09d' % rid
-        imgbuf = txn.get(img_code.encode())
-        img = np.frombuffer(imgbuf, dtype=np.uint8)
+        img_code: str = 'image-%09d' % index
+        img_buf = self._txn.get(img_code.encode())
+        img = np.frombuffer(img_buf, dtype=np.uint8)
         img = cv.imdecode(img, cv.IMREAD_COLOR)
         # img = cv.resize(img, (900, 32), interpolation=cv.INTER_CUBIC)
         img = normalize(img)
 
-        label_code: str = 'label-%09d' % rid
-        byte_label: bytes = txn.get(label_code.encode())
+        label_code: str = 'label-%09d' % index
+        byte_label: bytes = self._txn.get(label_code.encode())
         label = byte_label.decode("utf-8")
         label = label.strip("\n").strip("\r\t")
         label = self.alphabet.encode(label)
--- trainer.py
+++ trainer.py
@@ -185,7 +185,7 @@ if __name__ == "__main__":
     if args.alphabet.strip():
         config['alphabet']['path'] = args.alphabet.strip()
     if args.data.strip():
-        for item in ["train", "valid", "test"]:
+        for item in ["train", "valid"]:
             config[item]['dataset']['path'] = os.path.join(args.data.strip(), item, item)
     if args.resume.strip():
         config['checkpoint']['resume'] = args.resume.strip()
