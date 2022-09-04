ea0fa1cfcfe79acbd798ea8d2dc2e6a1f0e46a4c
wander217 <Trinhthinh2171999@gmail.com>
2022-09-04 23:06:57 +0700

    clean code

--- criterion/focal_loss.py
+++ criterion/focal_loss.py
@@ -14,7 +14,7 @@ class FocalLoss(nn.Module):
         target_ = target.contiguous().view(-1)
         pred_ = pred.view(-1, pred.size(-1))
         cross_entropy: Tensor = self._criterion(pred_, target_)
-        target_ = target_ * (target_ != self._pad).long()
-        input_prob: Tensor = torch.gather(F.softmax(pred_, 1), 1, target_.unsqueeze(1))
-        loss = torch.pow(1. - input_prob, self._gamma) * cross_entropy
-        return torch.mean(loss)
+        # target_ = target_ * (target_ != self._pad).long()
+        # input_prob: Tensor = torch.gather(F.softmax(pred_, 1), 1, target_.unsqueeze(1))
+        # loss = torch.pow(1. - input_prob, self._gamma) * cross_entropy
+        return torch.mean(cross_entropy)
